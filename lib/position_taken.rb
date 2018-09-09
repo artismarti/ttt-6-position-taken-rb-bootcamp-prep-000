6683ed2da963a8d56adcf8caf477d5be07e9026b
Arti M <artismarti@gmail.com>
2018-09-09 21:15:10 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,5 @@
-# code your #position_taken? method here!
 def position_taken?(board, index)
-  if (board == " " || board =="" || index == nil)
+  if (board == " " || board == "" || index == nil)
     false 
   end
-    
 end
