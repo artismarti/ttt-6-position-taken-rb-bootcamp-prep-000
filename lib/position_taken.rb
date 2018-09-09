0f2a50e4064b806360aa2d9f3c012ecd6de3a39c
Arti M <artismarti@gmail.com>
2018-09-09 21:18:38 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index)
   if (board[index] == nil || board[index] == " " || board[index] == "")
     false
-  elsif (board[index] == "X" || board[index] == "Y")
+  elsif (board[index] == "X" || board[index] == "O")
     true
   end
 end
